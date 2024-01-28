8a39157cdcff0cebbbf5abebd996cb0daf68c3cc
Merve Göncü <117481952+mervegonc@users.noreply.github.com>
2024-01-28 13:13:02 +0300

    Update LoginRequest.java

--- sSecurity/src/main/java/learn/process/sSecurity/LoginRequest.java
+++ sSecurity/src/main/java/learn/process/sSecurity/LoginRequest.java
@@ -1,25 +1,24 @@
 package learn.process.sSecurity;
 
+// Kullanıcı adı ve parola alanlarını içeren LoginRequest sınıfı.
 public class LoginRequest {
 
-	private String username;
-	private String password;
+    private String username;
+    private String password;
 
-	// Getter ve Setter metotları
+    // Kullanıcı adı getter ve setter metotları.
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    // Parola getter ve setter metotları.
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
